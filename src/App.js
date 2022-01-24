55ae142e5b2278e37d6ff1d7ab77c44971d4a676
Shiyu Wang <shiyu3169@gmail.com>
2022-01-23 17:24:04 -0800

    styled component hubble

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import { Container } from './components/styles/Container.styled'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './components/styles/Global'
+import content from './content'
+import Card from './components/Card'
+import Footer from './components/Footer'
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+  mobile: '768px',
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Header />
+      <Container className='container'>
+        {content.map((item) => (
+          <Card key={item.id} item={item} />
+        ))}
+      </Container>
+      <Footer />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
